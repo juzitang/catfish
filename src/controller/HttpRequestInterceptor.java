52681da183448e1a2b1d1c9f84aaacb656aeb3f4
zhanglihua <zhanglihua@(none)>
2012-02-20 19:50:46 +0800

    add interceptor

---
+++ src/controller/HttpRequestInterceptor.java
@@ -0,0 +1,40 @@
+package controller;
+
+import manager.ApplicationSecurityManager;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class HttpRequestInterceptor extends HandlerInterceptorAdapter {
+	
+	private ApplicationSecurityManager applicationSecurityManger;
+	private String loginPage;
+	
+	public ApplicationSecurityManager getApplicationSecurityManger() {
+		return applicationSecurityManger;
+	}
+
+	public void setApplicationSecurityManger(
+			ApplicationSecurityManager applicationSecurityManger) {
+		this.applicationSecurityManger = applicationSecurityManger;
+	}
+
+	public String getLoginPage() {
+		return loginPage;
+	}
+
+	public void setLoginPage(String loginPage) {
+		this.loginPage = loginPage;
+	}
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception
+		{
+			User user=applicationSecurityManger.getUser(request);
+			if(user==null){
+				response.sendRedirect(this.loginPage);
+				return false;
+			}
+			return true;
+		}
+
+}
---
+++ src/controller/LoginController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import manager.ApplicationSecurityManager;
+
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+public class LoginController extends SimpleFormController {
+	
+	private ApplicationSecurityManager applicationSecurityManager;
+
+	public ApplicationSecurityManager getApplicationSecurityManager() {
+		return applicationSecurityManager;
+	}
+
+	public void setApplicationSecurityManager(
+			ApplicationSecurityManager applicationSecurityManager) {
+		this.applicationSecurityManager = applicationSecurityManager;
+	}
+	
+	
+
+}
